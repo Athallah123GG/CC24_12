0bb38ae90d4f1fcdd50dbfe73979b6ae33e6aa66
okakakikioAxe <lukmanmuhamadilham@gmail.com>
2024-04-28 08:51:22 +0700

    1

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'user1',
+            'email' => 'user1@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('password'),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+    }
+}
