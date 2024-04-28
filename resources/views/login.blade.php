fbbc33a617e40c36fbf4aa8fb1ca386210fbdbb0
okakakikioAxe <lukmanmuhamadilham@gmail.com>
2024-04-28 14:49:07 +0700

    ud

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -79,7 +79,22 @@
             </div>
     </nav>
 
-
+    <section class="">
+        <form class="container w-25" >
+        <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Example@gmail.com">
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
+        </div>
+        <div class="form-group form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </section>
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
         integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
