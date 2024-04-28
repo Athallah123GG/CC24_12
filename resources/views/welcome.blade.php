0c702f5a6d3843c008cd98ac860a168916d58a19
wildanlomba <wildankcl89@gmail.com>
2024-04-27 22:04:32 -0700

    ok

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,7 +44,7 @@
             <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#">Products</a>
+            <a class="nav-link" href="product">Products</a>
         </li>
         <li class="nav-item">
             <a class="nav-link" href="#">Pricing</a>
